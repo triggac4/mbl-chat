dea4347724f26dbd70d99a3854dc1dad54568318
Kenechukwu Offokansi <koffokansi@gmail.com>
2025-03-04 09:32:19 +0100

    user controller

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -3,7 +3,7 @@ import User from "../models/userModel.js";
 import { generateToken } from "../utils/tokenUtil.js";
 import asyncHandler from "express-async-handler";
 
-const registerUser = asyncHandler(async (req, res) => {
+const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   const userExists = await User.findOne({ email });
@@ -20,15 +20,37 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-    generateToken(res, user._id);
+    const token = generateToken(user._id);
 
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
+      token,
     });
   } else {
     res.status(400);
     throw new Error("Invalid user data");
   }
+};
+//sign in
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  //password verify
+  if (user && (await user.matchPassword(password))) {
+    const token = generateToken(user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
+export { registerUser, loginUser };
